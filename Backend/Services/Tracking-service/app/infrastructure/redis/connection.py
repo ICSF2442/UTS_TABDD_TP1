2a28788e5ccc076ea5b1eae4d5c277b268836eb9
Ítalo Francisco <107189945+ICSF2442@users.noreply.github.com>
2025-11-10 20:52:02 +0000

    Update connection.py - remover host e password

--- Backend/Services/Tracking-service/app/infrastructure/redis/connection.py
+++ Backend/Services/Tracking-service/app/infrastructure/redis/connection.py
@@ -6,9 +6,9 @@ from app.core.config import settings
 class RedisConnection:
     def __init__(self):
         self.redis_client = redis.StrictRedis(
-            host="redis-14107.crce202.eu-west-3-1.ec2.redns.redis-cloud.com",
+            host="",
             port=14107,
-            password="9ReTNLIrZptjx2dE9hCG1163zOsS80yw",  # store in .env
+            password="",  # store in .env
             decode_responses=True
         )
 
